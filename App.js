01b3aaeea2e437bb43970c8719366ec0a5d7bc69
Huan Nguyen <huan06949@gmail.com>
2022-09-15 17:36:16 +0700

    login UI

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Login</Text>
+      <View>
+        <TextInput style={styles.textInput} placeholder="User Name" />
+      </View>
+      <View>
+        <TextInput style={styles.textInput} placeholder="Password" />
+      </View>
+      <View style={styles.button}>
+        <Button title="Login" />
+      </View>
+      <View style={styles.textBox}>
+        <Text style={styles.text}>Forget Password?</Text>
+        <Text style={styles.text}>Register?</Text>
+
+      </View>
     </View>
   );
 }
@@ -15,6 +28,38 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  textInput: {
+    padding: 15,
+    height: 60,
+    width: 350,
+    fontSize: 20,
+    borderWidth: 1,
+    marginBottom: 20,
+    color: 'black',
+  },
+  label: {
+    fontSize: 20,
+  },
+  title: {
+    color: 'black',
+    fontSize: 50,
+    margin: 60,
+  },
+  button: {
+    width: 350,
+    fontSize: 50,
+    height: 60,
+  },
+  textBox: {
+    marginTop: 100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 17,
+    color: "#06bcee",
+    marginBottom: 20,
+  }
 });
