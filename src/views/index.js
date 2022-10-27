60162585eb27f5ddc43250721246d1d782672938
Huan Nguyen <huan06949@gmail.com>
2022-10-27 14:50:10 +0700

    add Slides

--- src/views/index.js
+++ src/views/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, SafeAreaView, View } from "react-native";
 import Login from './Login';
 import HomePage from './home';
+import Slide from './Slides';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
@@ -9,10 +10,10 @@ export default RootComponent = () => {
   return (
     // <Login />
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName='Slide' screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={HomePage} />
-
+        <Stack.Screen name="Slide" component={Slide} />
       </Stack.Navigator>
     </NavigationContainer >
   )
